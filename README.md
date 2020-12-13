f4b6950ea9bdd574df6d3f80e245f7db858ecefd
patelaadi <74679464+patelaadi@users.noreply.github.com>
2020-12-13 12:07:42 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# The-Race
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,42 @@
+var wall;
+var lambo1, lambo2,lambo3, lambo4;
+var speed,weight;
+var rand;
+function setup() {
+  createCanvas(1600,400);
+
+  rand=random(10,50);
+
+  wall= createSprite(1500,200,60,400);
+  wall.shapeColor=("black");
+  speed= random(50,150);
+  weight= random(400,1500);
+
+  lambo1= createSprite(100,50,60,30);
+  lambo1.shapeColor=("crimson");
+  lambo2= createSprite(100,150,60,30);
+  lambo2.shapeColor=("lime");
+  lambo3= createSprite(100,250,60,30);
+  lambo3.shapeColor=("navy");
+  lambo4= createSprite(100,350,60,30);
+  lambo4.shapeColor=("magenta");
+}
+
+function draw() {
+  background(0,128,128); 
+  lambo1.velocityX=speed+rand;
+  lambo2.velocityX=speed+rand;
+  lambo3.velocityX=speed+rand;
+  lambo4.velocityX=speed+rand; 
+  if(collision(lambo1,wall)){
+    lambo1.velocityX=0;
+    lambo2.velocityX=0;
+    lambo3.velocityX=0;
+    lambo4.velocityX=0;
+  }
+ 
+  drawSprites();
+}
+
+
+
